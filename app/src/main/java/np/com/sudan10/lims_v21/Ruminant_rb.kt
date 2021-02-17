0da4c64d13a425799aeacdd2805a4b308b6e4098
sudan23 <pudasaini.sudan10@gmail.com>
2021-02-17 08:25:25 +0545

    Non Ruminant design Finalized

--- app/src/main/java/np/com/sudan10/lims_v21/Ruminant_rb.kt
+++ app/src/main/java/np/com/sudan10/lims_v21/Ruminant_rb.kt
@@ -14,7 +14,7 @@ class Ruminant_rb : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ruminant_rb, container, false)
+        return inflater.inflate(R.layout.fragment_ruminant, container, false)
     }
 
 }
