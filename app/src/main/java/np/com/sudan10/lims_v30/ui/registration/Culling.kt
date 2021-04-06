156d59bb1d3f4a24a9e58e22f7b91556666d0b63
sudan23 <pudasaini.sudan10@gmail.com>
2021-04-06 13:05:12 +0545

    Culling Form

---
+++ app/src/main/java/np/com/sudan10/lims_v30/ui/registration/Culling.kt
@@ -0,0 +1,18 @@
+package np.com.sudan10.lims_v30.ui.registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import np.com.sudan10.lims_v30.R
+
+class Culling : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_culling, container, false)
+    }
+
+}
