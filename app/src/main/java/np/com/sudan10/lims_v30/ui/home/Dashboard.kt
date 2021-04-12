83d3ac4bdc5882fab6c4c3e5b2adfb67f7bf501f
sudan23 <pudasaini.sudan10@gmail.com>
2021-04-12 16:22:35 +0545

    Dashboard protopype

---
+++ app/src/main/java/np/com/sudan10/lims_v30/ui/home/Dashboard.kt
@@ -0,0 +1,32 @@
+package np.com.sudan10.lims_v30.ui.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import np.com.sudan10.lims_v30.R
+
+class Dashboard : Fragment() {
+
+    companion object {
+        fun newInstance() = Dashboard()
+    }
+
+    private lateinit var viewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/np/com/sudan10/lims_v30/ui/home/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package np.com.sudan10.lims_v30.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
