bdac0ed1e950922f068e4267b6599056d52b164f
sudan23 <pudasaini.sudan10@gmail.com>
2021-06-01 15:29:22 +0545

    Logout page added

--- app/src/main/java/np/com/sudan10/lims_v30/ui/auth/Logout.kt
+++ app/src/main/java/np/com/sudan10/lims_v30/ui/auth/Logout.kt
@@ -1,26 +1,18 @@
 package np.com.sudan10.lims_v30.ui.auth
 
 
-import android.util.Base64
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import np.com.sudan10.lims_v30.R
 import np.com.sudan10.lims_v30.data.network.AuthApi
-import np.com.sudan10.lims_v30.data.network.Resource
 import np.com.sudan10.lims_v30.data.repository.AuthRepository
-import np.com.sudan10.lims_v30.databinding.FragmentLoginBinding
+import np.com.sudan10.lims_v30.databinding.FragmentLogoutBinding
 import np.com.sudan10.lims_v30.ui.base.BaseFragment
 import np.com.sudan10.lims_v30.ui.home.Home
-import np.com.sudan10.lims_v30.util.enable
-import np.com.sudan10.lims_v30.util.hide
-import np.com.sudan10.lims_v30.util.show
 import np.com.sudan10.lims_v30.util.startNewActivity
 
 
-class Logout : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>() {
+class Logout : BaseFragment<AuthViewModel, FragmentLogoutBinding, AuthRepository>() {
 
     override fun onResume() {
         super.onResume()
@@ -39,7 +31,7 @@ class Logout : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>
 
     override fun getViewModel() = AuthViewModel::class.java
 
-    override fun getFragmentBinding() = R.layout.fragment_login
+    override fun getFragmentBinding() = R.layout.fragment_logout
 
     override fun getFragmentRepository() = AuthRepository(remoteDataSource.buildApi(AuthApi::class.java))
     }
